4b04ccc3d2f8df5d86bb235e32fed55522c288b4
AlexTankerD <sasha.golovin.0976@gmail.com>
2023-05-16 02:46:25 +0400

    Добавьте файлы проекта.

---
+++ DebugPrg/Program.cs
@@ -0,0 +1,7 @@
+﻿using SF2022User_NN_Lib;
+
+TimeSpan[] startTime = new TimeSpan[] { new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0),
+               new TimeSpan(15, 0, 0) ,new TimeSpan(15, 30, 0), new TimeSpan(16, 50, 0) };
+int[] duration = { 60, 30, 10, 10, 40 };
+var actual = Calculations.AvailablePeriods(startTime, duration, 30, new TimeSpan(8, 0, 0), new TimeSpan(18, 0, 0));
+        
---
+++ SF2022User{NN}Lib/Calculations.cs
@@ -0,0 +1,32 @@
+﻿namespace SF2022User_NN_Lib
+{
+    public static class Calculations
+    {
+        public static string[] AvailablePeriods(TimeSpan[] startTime, int[] durations, 
+            int consultationtime, TimeSpan beginWorkingTime, TimeSpan endWorkingTime)
+        {
+            TimeSpan time1 = beginWorkingTime;
+            TimeSpan time2 = time1;
+            int stringorder = 0;
+            List<string> periods = new List<string>();
+
+            for(int i = 0; i < startTime.Length && i < durations.Length  ; i++) 
+            {
+                for (; time1 <= endWorkingTime; time1 += new TimeSpan(0,consultationtime,0))
+                {
+                    
+                    if (time1 >= startTime[i] && time1 <=startTime[i] + new TimeSpan(0, durations[i], 0))
+                    {
+                        time1 = startTime[i] + new TimeSpan(0, durations[i], 0);
+                        break;
+                        
+                    }
+                    periods.Add($"{time1}-{time2 = time1 + new TimeSpan(0, consultationtime, 0)}");
+                    stringorder++;
+                }
+
+            }
+            return periods.ToArray();
+        }
+    }
+}
